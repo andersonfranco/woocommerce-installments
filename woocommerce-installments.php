fa580847ec3a2fb1a28d847587e7532509e53256
Anderson Franco <anderson@francotecnologia.com>
2014-12-26 19:25:39 -0200

    OOP structure

--- woocommerce-installments.php
+++ woocommerce-installments.php
@@ -5,224 +5,235 @@ Plugin URI: https://github.com/AndersonFranco/woocommerce-installments
 Description: This plugin appends installments into the product price.
 Author: Anderson Franco
 Author URI: http://www.francotecnologia.com/
-Version: 1.2.1
+Version: 1.3.0
 License: GPLv2 or later
 */
 
-// ONLY PRODUCTS WITH PRICE GREATER THAN OR EQUAL TO:
-define('FRANCOTECNOLOGIA_WC_PARCPAGSEG_PRICE_GTET', 0); // e.g. 1000
+class FrancoTecnologiaWooCommerceInstallments {
 
-// MINIMUM MONTHLY PAYMENT - MUST BE GREATER THAN ZERO:
-define('FRANCOTECNOLOGIA_WC_PARCPAGSEG_MINIMUM_MONTHLY_PAYMENT', 5); // e.g. 5
+  // ONLY PRODUCTS WITH PRICE GREATER THAN OR EQUAL TO:
+  protected static $priceGreaterThanOrEqualTo = 0;
 
-// NUMBER OF THE COLUMNS OF THE TABLE:
-define('FRANCOTECNOLOGIA_WC_PARCPAGSEG_TABLE_COLUMNS', 2);
+  // MINIMUM MONTHLY PAYMENT - MUST BE GREATER THAN ZERO:
+  protected static $minimumMonthlyPayment     = 5;
 
-// ADD TO CART - BUTTON POSITION: TOP = true, BOTTOM = false
-define('FRANCOTECNOLOGIA_WC_PARCPAGSEG_ADD_TO_CART_BUTTON_POSITION', false);
+  // NUMBER OF THE COLUMNS OF THE TABLE:
+  protected static $numberOfTableColumns      = 2;
 
-// USE COEFFICIENT TABLE / INTEREST RATES - LINE 55:
-define('FRANCOTECNOLOGIA_WC_PARCPAGSEG_USE_COEFFICIENT_TABLE', false);
+  // ADD TO CART - BUTTON POSITION: TOP = true, BOTTOM = false
+  protected static $addToCartButtonPosition   = false;
 
-// CART PAGE MESSAGE:
-// ADD %d TO SHOW MAX INSTALLMENTS ALLOWED
-// OR LEAVE EMPTY FOR NO MESSAGE
-define('FRANCOTECNOLOGIA_WC_PARCPAGSEG_CART_PAGE_MESSAGE', 
-  'NO interest for %d months' // e.g. (Portuguese) * Pague sua compra em at&eacute; %d vezes
-);
+  // USE COEFFICIENT TABLE / INTEREST RATES:
+  protected static $useCoefficientTable       = false;
 
-// Translate the words below in your own language:
-// or (e.g. ou)
-// Installments (e.g. Parcelas)
-// Amount (e.g. Valor)
-// InStock (e.g. Em estoque)
-// OutOfStock (e.g. Sem estoque)
+  // COEFFICIENT TABLE - INTEREST RATES OF PAGSEGURO.COM.BR:
+  protected static $coefficientTable = array( 
+    1, 0.52255, 0.35347, 
+    0.26898, 0.21830, 0.18453, 
+    0.16044, 0.14240, 0.12838, 
+    0.11717, 0.10802, 0.10040
+  );
 
-// // // // // // // // // // // // // // // // // // // // // // // // // // //
+  // USE DICTIONARY LANGUAGE (DEFAULT: ENGLISH)
+  protected static $useDictLanguage = true;
 
-function francotecnologia_wc_parcpagseg_calculate_installment($price = 0.00, $installment = 0) {
+  // IF $useDictLanguage == false, USE THIS WORDS:
+  protected static $language = array(
+    'or'              => 'ou',
+    'Installments'    => 'Parcelas',
+    'Amount'          => 'Valor',
+    'InStock'         => 'Em estoque',
+    'OutOfStock'      => 'Indisponível',
+    'cartPageMessage' => 'Pague em at&eacute; %d vezes'
+  );
 
-  $price        = (float) $price;
-  $installment  = (int) $installment;
-  $result       = new stdClass(); 
+  public static function init() {
 
-  if ($installment < 1 || $installment > 12) {
-    $result->price = 0;
-    $result->total = 0;
-  } else if (FRANCOTECNOLOGIA_WC_PARCPAGSEG_USE_COEFFICIENT_TABLE) {
-    
-    // INTEREST RATES OF PAGSEGURO.COM.BR
-    $coefficient = array( 
-      1, 0.52255, 0.35347, 
-      0.26898, 0.21830, 0.18453, 
-      0.16044, 0.14240, 0.12838, 
-      0.11717, 0.10802, 0.10040
-    );
-
-    $result->price = sprintf("%0.2f", $price * $coefficient[$installment - 1]);
-    $result->total = sprintf("%0.2f", ($price * $coefficient[$installment - 1]) * $installment);
-  } else {
-    $result->price = sprintf("%0.2f", ($price / $installment));
-    $result->total = sprintf("%0.2f", ($price / $installment) * $installment);
-  }
-
-  return $result;
-}
-
-function francotecnologia_wc_parcpagseg_get_price($price = null) {
-  if ($price === null) {
-    $product = get_product();
-    if ($product->get_price()) {
-      $price = $product->get_price();
+    if (static::$useDictLanguage) {
+      // Default English words
+      static::$language = array(
+        'or'              => __('or'),
+        'Installments'    => __('Installments'),
+        'Amount'          => __('Amount'),
+        'InStock'         => __('InStock'),
+        'OutOfStock'      => __('OutOfStock'),
+        'cartPageMessage' => __('NO interest for %d months')
+      );
     }
-  }  
-  return $price;
-}
 
-function francotecnologia_wc_parcpagseg_get_installments($price = 0.00) {
-  $installments = round($price / FRANCOTECNOLOGIA_WC_PARCPAGSEG_MINIMUM_MONTHLY_PAYMENT);
-  if ($installments > 12) {
-    $installments = 12;
-  } else if ($installments < 1) { 
-    $installments = 1;
+    add_action('plugins_loaded', array(get_called_class(), 'actions'));
   }
-  return $installments;
-}
 
-function francotecnologia_wc_parcpagseg_get_parceled_value($price = null) {
-  $price = francotecnologia_wc_parcpagseg_get_price($price);
-  if ($price > 0) {
-    $installments = francotecnologia_wc_parcpagseg_get_installments($price);
-    $calc = francotecnologia_wc_parcpagseg_calculate_installment($price, $installments);
-    return $installments . 'x ' . wc_price($calc->price);
-  } else {
-    return '';
-  }
-}
-
-function francotecnologia_wc_parcpagseg_get_parceled_table($price = null, $variation_id = null, $variation_display = null) {
-  $price = francotecnologia_wc_parcpagseg_get_price($price);
-  if ($price > 0) {
-    $installments = francotecnologia_wc_parcpagseg_get_installments($price);
-    $table = '<table class="francotecnologia_wc_parcpagseg_table ';
-    $table .= 'francotecnologia_wc_parcpagseg_table_with_variation_id_' . ($variation_id > 0 ? $variation_id : '0') . '" ';
-    $table .= ($variation_display === false ? 'style="display:none"' : '');
-    $table .= '><tr>';
-    $table .= str_repeat('<th>' . __('Installments') . '</th><th>' . __('Amount') . '</th>', FRANCOTECNOLOGIA_WC_PARCPAGSEG_TABLE_COLUMNS);
-    $table .= '</tr>';
-    $tdCounter = 0;
-    foreach (range(1, $installments) as $parcel) {
-      $calc = francotecnologia_wc_parcpagseg_calculate_installment($price, $parcel);
-      $tdCounter = 1 + $tdCounter % FRANCOTECNOLOGIA_WC_PARCPAGSEG_TABLE_COLUMNS;
-      if ($tdCounter == 1) {
-        $table .= '<tr>';
-      }      
-      $table .= '<th>' . $parcel . '</th><td>' . wc_price($calc->price) . '</td>';
-      if ($tdCounter == FRANCOTECNOLOGIA_WC_PARCPAGSEG_TABLE_COLUMNS) {
-        $table .= '</tr>';
-      }      
+  public static function actions() {
+    // Product Page
+    remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_price', 10);
+    add_action('woocommerce_single_product_summary', array(get_called_class(), 'actionSingleProduct'), ((static::$addToCartButtonPosition)?30:10));
+    // Catalog
+    add_action('woocommerce_after_shop_loop_item_title', array(get_called_class(), 'actionLoopItem'), 20);
+    // Cart
+    if (static::$language['cartPageMessage'] != '') {
+      add_action('woocommerce_cart_totals_after_order_total', array(get_called_class(), 'actionCart'), 20);
     }
-    if (substr( $table, -5 ) != '</tr>') {
-      $table .= '</tr>';
-    }    
-    $table .= '</table>';
-    return $table;
-  } else {
-    return '';
+    // CSS 
+    add_action('wp_enqueue_scripts', array(get_called_class(), 'css'), 98);
+    // Javascript 
+    add_action('wp_enqueue_scripts', array(get_called_class(), 'js'), 99);
   }
-}
 
-function francotecnologia_wc_parcpagseg_loop_item() {
-  if (francotecnologia_wc_parcpagseg_get_price() >= FRANCOTECNOLOGIA_WC_PARCPAGSEG_PRICE_GTET) {
-    echo ' <span class="price francotecnologia_wc_parcpagseg_loop_item_price_span">' 
-         . (francotecnologia_wc_parcpagseg_get_price() > 0 ? __('or') . ' ' : '')
-         . francotecnologia_wc_parcpagseg_get_parceled_value() . '</span>';
-  }
-}
+  protected static function calculateInstallment($price = 0.00, $installment = 0) {
+    $price        = (float) $price;
+    $installment  = (int) $installment;
+    $result       = new stdClass(); 
 
-function francotecnologia_wc_parcpagseg_single_product() {
-  if (francotecnologia_wc_parcpagseg_get_price() < FRANCOTECNOLOGIA_WC_PARCPAGSEG_PRICE_GTET) {
-    woocommerce_template_single_price();
-    return;
-  }
-  $product = get_product();
-  ?>
-  <div itemprop="offers" itemscope itemtype="http://schema.org/Offer">
-    <p class="price"><?php echo $product->get_price_html(); ?> 
-      <span class="francotecnologia_wc_parcpagseg_single_product_price_span">
-        <?php echo (francotecnologia_wc_parcpagseg_get_price() > 0 ? __('or') . ' ' : '') . francotecnologia_wc_parcpagseg_get_parceled_value(); ?>
-      </span>
-    </p>
-    <?php 
-      if ($product->product_type != 'variable') {
-        echo francotecnologia_wc_parcpagseg_get_parceled_table();
-      } else {
-        $variationList = $product->get_available_variations();
-        foreach($variationList AS $variation) {
-          $productVariation = new WC_Product_Variation($variation['variation_id']);
-          $defaultVariation = array_diff($variation['attributes'], $product->get_variation_default_attributes());
-          echo francotecnologia_wc_parcpagseg_get_parceled_table($productVariation->get_price(), $variation['variation_id'], empty($defaultVariation));
-        }
-      }
-    ?>
-    <meta itemprop="price" content="<?php echo $product->get_price(); ?>" />
-    <meta itemprop="priceCurrency" content="<?php echo get_woocommerce_currency(); ?>" />
-    <link itemprop="availability" href="http://schema.org/<?php echo $product->is_in_stock() ? __('InStock') : __('OutOfStock'); ?>" />
-  </div>
-  <?php
-}
-
-function francotecnologia_wc_parcpagseg_cart() {
-  global $woocommerce;
-  if ($woocommerce->cart->total < FRANCOTECNOLOGIA_WC_PARCPAGSEG_PRICE_GTET) {
-    return;
-  }
-  if ($woocommerce->cart->total) {
-    $installments = francotecnologia_wc_parcpagseg_get_installments($woocommerce->cart->total);
-  } else {
-    $installments = 0;
-  }  
-  if (stripos(FRANCOTECNOLOGIA_WC_PARCPAGSEG_CART_PAGE_MESSAGE,'%d') !== false) {
-    if ($installments > 0) {
-      $message = sprintf(FRANCOTECNOLOGIA_WC_PARCPAGSEG_CART_PAGE_MESSAGE, $installments);
+    if ($installment < 1 || $installment > 12) {
+      $result->price = 0;
+      $result->total = 0;
+    } else if (static::$useCoefficientTable) {      
+      $result->price = sprintf("%0.2f", $price * static::$coefficientTable[$installment - 1]);
+      $result->total = sprintf("%0.2f", ($price * static::$coefficientTable[$installment - 1]) * $installment);
     } else {
-      $message = '';
+      $result->price = sprintf("%0.2f", ($price / $installment));
+      $result->total = sprintf("%0.2f", ($price / $installment) * $installment);
     }
-  } else {
-    $message = FRANCOTECNOLOGIA_WC_PARCPAGSEG_CART_PAGE_MESSAGE;
-  } 
-  ?>
-  <tr><th colspan="2" class="francotecnologia_wc_parcpagseg_cart_tr_th"><?php echo $message; ?></th></tr>
-  <?php
-}
 
-function francotecnologia_wc_parcpagseg_css() {
-  wp_enqueue_style('woocommerce-installments', plugins_url('woocommerce-installments.css', __FILE__), array(), '1.0', 'all');
-}
-
-function francotecnologia_wc_parcpagseg_js() {
-  wp_enqueue_script('woocommerce-installments', plugins_url('woocommerce-installments.js', __FILE__), array('jquery', 'wc-add-to-cart-variation'), '1.0', true);
-}
-
-function francotecnologia_wc_parcpagseg_alter_woo_hooks() {
-  // Product Page
-  remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_price', 10);  
-  add_action('woocommerce_single_product_summary', 'francotecnologia_wc_parcpagseg_single_product', 
-    ((FRANCOTECNOLOGIA_WC_PARCPAGSEG_ADD_TO_CART_BUTTON_POSITION)?30:10));
-
-  // Catalog
-  add_action('woocommerce_after_shop_loop_item_title', 'francotecnologia_wc_parcpagseg_loop_item', 20);
-
-  // Cart
-  if (FRANCOTECNOLOGIA_WC_PARCPAGSEG_CART_PAGE_MESSAGE != '') {
-    add_action('woocommerce_cart_totals_after_order_total', 'francotecnologia_wc_parcpagseg_cart', 20);
+    return $result;
   }
 
-  // CSS 
-  add_action('wp_enqueue_scripts', 'francotecnologia_wc_parcpagseg_css', 98);
+  protected static function getPrice($price = null) {
+    if ($price === null) {
+      $product = get_product();
+      if ($product->get_price()) {
+        $price = $product->get_price();
+      }
+    }  
+    return $price;
+  }
 
-  // Javascript 
-  add_action('wp_enqueue_scripts', 'francotecnologia_wc_parcpagseg_js', 99);
+  protected static function getInstallments($price = 0.00) {
+    $installments = round($price / static::$minimumMonthlyPayment);
+    if ($installments > 12) {
+      $installments = 12;
+    } else if ($installments < 1) { 
+      $installments = 1;
+    }
+    return $installments;
+  }
+
+  protected static function getParceledValue($price = null) {
+    $price = static::getPrice($price);
+    if ($price > 0) {
+      $installments = static::getInstallments($price);
+      $calc         = static::calculateInstallment($price, $installments);
+      return $installments . 'x ' . wc_price($calc->price);
+    } else {
+      return '';
+    }
+  }
+
+  protected static function getParceledTable($price = null, $variation_id = null, $variation_display = null) {
+    $price = static::getPrice($price);
+    if ($price > 0) {
+      $installments = static::getInstallments($price);
+      $table = '<table class="francotecnologia_wc_parcpagseg_table ';
+      $table .= 'francotecnologia_wc_parcpagseg_table_with_variation_id_' . ($variation_id > 0 ? $variation_id : '0') . '" ';
+      $table .= ($variation_display === false ? 'style="display:none"' : '');
+      $table .= '><tr>';
+      $table .= str_repeat('<th>' . static::$language['Installments'] . '</th><th>' . static::$language['Amount'] . '</th>', static::$numberOfTableColumns);
+      $table .= '</tr>';
+      $tdCounter = 0;
+      foreach (range(1, $installments) as $parcel) {
+        $calc = static::calculateInstallment($price, $parcel);
+        $tdCounter = 1 + $tdCounter % static::$numberOfTableColumns;
+        if ($tdCounter == 1) {
+          $table .= '<tr>';
+        }      
+        $table .= '<th>' . $parcel . '</th><td>' . wc_price($calc->price) . '</td>';
+        if ($tdCounter == static::$numberOfTableColumns) {
+          $table .= '</tr>';
+        }      
+      }
+      if (substr( $table, -5 ) != '</tr>') {
+        $table .= '</tr>';
+      }    
+      $table .= '</table>';
+      return $table;
+    } else {
+      return '';
+    }
+  }
+
+  public static function actionLoopItem() {
+    if (static::getPrice() >= static::$priceGreaterThanOrEqualTo) {
+      echo ' <span class="price francotecnologia_wc_parcpagseg_loop_item_price_span">' 
+           . (static::getPrice() > 0 ? static::$language['or'] . ' ' : '')
+           . static::getParceledValue() . '</span>';
+    }
+  }
+
+  public static function actionSingleProduct() {
+    if (static::getPrice() < static::$priceGreaterThanOrEqualTo) {
+      woocommerce_template_single_price();
+      return;
+    }
+    $product = get_product();
+    ?>
+    <div itemprop="offers" itemscope itemtype="http://schema.org/Offer">
+      <p class="price"><?php echo $product->get_price_html(); ?> 
+        <span class="francotecnologia_wc_parcpagseg_single_product_price_span">
+          <?php echo (static::getPrice() > 0 ? static::$language['or'] . ' ' : '') . static::getParceledValue(); ?>
+        </span>
+      </p>
+      <?php 
+        if ($product->product_type != 'variable') {
+          echo static::getParceledTable();
+        } else {
+          $variationList = $product->get_available_variations();
+          foreach($variationList AS $variation) {
+            $productVariation = new WC_Product_Variation($variation['variation_id']);
+            $defaultVariation = array_diff($variation['attributes'], $product->get_variation_default_attributes());
+            echo static::getParceledTable($productVariation->get_price(), $variation['variation_id'], empty($defaultVariation));
+          }
+        }
+      ?>
+      <meta itemprop="price" content="<?php echo $product->get_price(); ?>" />
+      <meta itemprop="priceCurrency" content="<?php echo get_woocommerce_currency(); ?>" />
+      <link itemprop="availability" href="http://schema.org/<?php echo $product->is_in_stock() ? static::$language['InStock'] : static::$language['OutOfStock']; ?>" />
+    </div>
+    <?php
+  }
+
+  public static function actionCart() {
+    global $woocommerce;
+    if ($woocommerce->cart->total < static::$priceGreaterThanOrEqualTo) {
+      return;
+    }
+    if ($woocommerce->cart->total) {
+      $installments = static::getInstallments($woocommerce->cart->total);
+    } else {
+      $installments = 0;
+    }  
+    if (stripos(static::$language['cartPageMessage'],'%d') !== false) {
+      if ($installments > 0) {
+        $message = sprintf(static::$language['cartPageMessage'], $installments);
+      } else {
+        $message = '';
+      }
+    } else {
+      $message = static::$language['cartPageMessage'];
+    } 
+    ?>
+    <tr><th colspan="2" class="francotecnologia_wc_parcpagseg_cart_tr_th"><?php echo $message; ?></th></tr>
+    <?php
+  }
+
+  public static function css() {
+    wp_enqueue_style('woocommerce-installments', plugins_url('woocommerce-installments.css', __FILE__), array(), '1.0', 'all');
+  }
+
+  public static function js() {
+    wp_enqueue_script('woocommerce-installments', plugins_url('woocommerce-installments.js', __FILE__), array('jquery', 'wc-add-to-cart-variation'), '1.0', true);
+  }
+
 }
 
-add_action('plugins_loaded','francotecnologia_wc_parcpagseg_alter_woo_hooks');
+FrancoTecnologiaWooCommerceInstallments::init();
